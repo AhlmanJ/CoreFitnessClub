03941725e4c272bba7da1364a653bfb314ee4262
AhlmanJ <johan.ahlman@hotmail.com>
2026-03-05 13:46:42 +0100

    Add project files.

---
+++ Presentation.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Home()
+        {
+            ViewData["Tilte"] = "Home";
+
+            return View();
+        }
+    }
+}

8fa7a38dcd40477f927263fb8aa498f7cbbbd721
AhlmanJ <johan.ahlman@hotmail.com>
2026-03-18 12:13:00 +0100

    Added the Customer Service page.

---
+++ Presentation.WebApp/Controllers/CustomerServiceController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.WebApp.Controllers
+{
+    public class CustomerServiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "CustomerService";
+
+            return View();
+        }
+    }
+}
